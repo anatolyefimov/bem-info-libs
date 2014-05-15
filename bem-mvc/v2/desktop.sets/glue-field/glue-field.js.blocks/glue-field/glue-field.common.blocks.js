935d7e776dbf45551e9020c25620dd06a9e6e386
Bemer <bemer@yandex-team.ru>
2014-05-16 00:16:35 +0400

    Update data: Fri May 16 2014 00:16:35 GMT+0400 (MSK)

---
+++ bem-mvc/v2/desktop.sets/glue-field/glue-field.js.blocks/glue-field/glue-field.common.blocks.js
@@ -0,0 +1,1 @@
+/Users/user/Work/bds-bem-info/content/bem-mvc/v2/common.blocks/glue-field/glue-field.js
---
+++ bem-mvc/v2/desktop.sets/model/model.js.blocks/model/__field/_type/model__field_type_string.common.blocks.js
@@ -0,0 +1,1 @@
+/Users/user/Work/bds-bem-info/content/bem-mvc/v2/common.blocks/model/__field/_type/model__field_type_string.js
